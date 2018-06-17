384b63758704e98304c21e7cc346c541289049c9
Random User <randstuff@protonmail.com>
2018-06-17 02:25:52 +0200

    .

--- files/iptables.save.rule.sh
+++ files/iptables.save.rule.sh
@@ -1,5 +1,8 @@
 #!/bin/sh 
 
+
+echo "sudo su -c 'ip6tables-save > /etc/iptables/rules.v6'" 
 echo "sudo su -c 'iptables-save > /etc/iptables/rules.v4' \n\n" 
 sudo su -c 'iptables-save > /etc/iptables/rules.v4'
+sudo su -c 'ip6tables-save > /etc/iptables/rules.v6'
 
